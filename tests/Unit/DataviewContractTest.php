3d5b5180c9ec5ab7e07ff3b26642278e2dbc7281
Rim Zabarov <rim@zabarov.com>
2026-07-07 16:03:04 +0300

    Fix dataview unit test scripts

--- tests/Unit/DataviewContractTest.php
+++ tests/Unit/DataviewContractTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+require_once __DIR__ . '/../../vendor/autoload.php';
+
 use Larena\Dataview\Contracts\DataviewActionPolicy;
 use Larena\Dataview\Contracts\DataviewFieldDescriptor;
 use Larena\Dataview\Contracts\DataviewSavedView;
